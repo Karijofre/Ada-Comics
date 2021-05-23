b6014f68871fd66d7af7b210694dc1881288f065
karijofre <karinajofre@hotmail.com>
2021-05-24 00:36:28 +0200

    Subiendo maquetado de figuras y paginación

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+//Print of comics
+
+const base = document.getElementById("base");
+
 const printData = arr =>{
-    console.log(arr);
+    //console.log(arr);
+    const notViable = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available";
+    const replacementViable = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/portrait_uncanny";
+    let box ="";
+    arr.forEach(comic => {
+       console.log(comic);
+       const {title, thumbnail:{extension, path}, id} = comic;
+       box += `
+       <div class="column is-one-fifth" onclick="getId(${id})">
+        <figure>
+         <a>
+          <img src="${path === notViable ? replacementViable: path}.${extension}" alt="${title}">
+          <p>${title}</p>
+         </a>
+         </figure>
+       </div>
+       `
+    });
+    base.innerHTML = box;
 }
